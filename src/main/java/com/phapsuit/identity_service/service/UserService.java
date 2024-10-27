d97cf0f9ca83abe07bccce18839100ce7fb7d2c0
Phim Muot <phimmuot@gmail.com>
2024-10-27 20:11:59 +0700

    Initial Project

---
+++ src/main/java/com/phapsuit/identity_service/service/UserService.java
@@ -0,0 +1,56 @@
+package com.phapsuit.identity_service.service;
+
+import com.phapsuit.identity_service.dto.request.UserCreationRequest;
+import com.phapsuit.identity_service.dto.request.UserUpdateRequest;
+import com.phapsuit.identity_service.dto.response.UserResponse;
+import com.phapsuit.identity_service.entity.User;
+import com.phapsuit.identity_service.exception.AppException;
+import com.phapsuit.identity_service.exception.ErrorCode;
+import com.phapsuit.identity_service.mapper.UserMapper;
+import com.phapsuit.identity_service.repository.UserRepository;
+import lombok.AccessLevel;
+import lombok.RequiredArgsConstructor;
+import lombok.experimental.FieldDefaults;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE, makeFinal = true)
+public class UserService {
+    UserRepository userRepository;
+    UserMapper userMapper;
+
+    public User createUser(UserCreationRequest request) {
+
+        if (userRepository.existsByUsername(request.getUsername()))
+            throw new AppException(ErrorCode.USER_EXISTED);
+
+        User user = userMapper.toUser(request);
+
+        return userRepository.save(user);
+    }
+
+    public UserResponse updateUser(String userId, UserUpdateRequest request) {
+        User user = userRepository.findById(userId).orElseThrow(() -> new RuntimeException("User not found"));
+
+        userMapper.updateUser(user, request);
+
+        return userMapper.toUserResponse(userRepository.save(user));
+    }
+
+    public void deleteUser(String userId) {
+        userRepository.deleteById(userId);
+    }
+
+    public List<User> getUsers() {
+        return userRepository.findAll();
+    }
+
+    public UserResponse getUser(String id) {
+        return userMapper.toUserResponse(userRepository.findById(id)
+                .orElseThrow(() -> new RuntimeException("User not found")));
+    }
+}
